0aa561b808791de19ce867756a951f53be38b07d
John-Henry Ross <johnhenryr@gmail.com>
2019-02-04 22:04:32 +0200

    whack a mole cleanup and bankacc gui
    deleted main duplicate from whack a mole project, started coding gui for bankacc project.

--- bankacc/main.py
+++ bankacc/main.py
@@ -1,32 +1,35 @@
 from bankaccount import BankAcc
 import mysql.connector
+import tkinter as tk
 
-def dbfunc():
-    mydb = mysql.connector.connect(
-        host="sql1.jnb3.host-h.net",
-        user="kieketehht_4",
-        passwd="uVv4MZ4rER6NEfAaKtb8",
-        database="kieketehht_pyt"
-    )
-    try:
-        mycursor = mydb.cursor()
-        mycursor.execute("USE kieketehht_pyt")
-        mycursor.execute("SHOW TABLES")
-        results  = mycursor.fetchall()
-        print("All Tables currently in Database: ",results)
-        results_list = [item[0] for item in results]
 
-        if "users" not in results_list:
-            mycursor.execute("CREATE TABLE users ( id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),password VARCHAR(255))")
-            print("Users table created")
-        else:
-            print("Users table already exist")
 
-    except Exception as ex:
-        errormessage = "An exception of type {0} occurred. Arguments: \n{1!r}"
-        message = errormessage.format(type(ex).__name__,ex.args)
-        print("There was an issue connecting to your database, please try again later")
-        print(message)
+mydb = mysql.connector.connect(
+    host="sql1.jnb3.host-h.net",
+    user="kieketehht_4",
+    passwd="uVv4MZ4rER6NEfAaKtb8",
+    database="kieketehht_pyt"
+)
+try:
+    mycursor = mydb.cursor()
+
+    mycursor.execute("USE kieketehht_pyt")
+    mycursor.execute("SHOW TABLES")
+    results  = mycursor.fetchall()
+    print("All Tables currently in Database: ",results)
+    results_list = [item[0] for item in results]
+
+    if "users" not in results_list:
+        mycursor.execute("CREATE TABLE users ( id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),password VARCHAR(255), account_no VARCHAR(200), amount FLOAT)")
+        print("Users table created")
+    else:
+        print("Users table already exist. Moving on.")
+
+except Exception as ex:
+    errormessage = "An exception of type {0} occurred. Arguments: \n{1!r}"
+    message = errormessage.format(type(ex).__name__,ex.args)
+    print("There was an issue connecting to your database, please try again later")
+    print(message)
 
 
 
@@ -38,9 +41,55 @@ def choice_validate(value: str):
         value = input("Invalid choice. Please try again. Y/N: ")
     return value
 
+def all_children(window):
+    _list = window.winfo_children()
+    for item in _list:
+        if item.winfo_children():
+            _list.extend(item.winfo_children())
+    return _list
+
+def account_create():
+
+    widget_list = all_children(win)
+    for item in widget_list:
+        item.pack_forget()
+
+    tk.Label(win,text="Please enter the account holder name").pack()
+    username = tk.Entry(win)
+    username.pack()
+
+    tk.Label(win,text="Please enter your password").pack()
+    password = tk.Entry(win,show="*")
+    password.pack()
+
+    tk.Label(win,text="Please enter the amount that you want to deposit")
+    amount = tk.Entry(win)
+    amount.pack()
+
+    tk.Button(win,text="Create Account",command=lambda: create_user(username,password,amount)).pack()
+
+def create_user(username,password,amount):
+
+    mycursor = mydb.cursor()
+    command = "INSERT INTO users(name,password,amount) VALUES('%s','%s','%d')" %(username.get(),password.get(),int(amount.get()))
+    mycursor.execute(command)
+    print(username.get())
+    print(password.get())
+    mydb.commit()
+
+win = tk.Tk()
+win.title("Banking system")
+tk.Label(win,text="Welcome to the Banking system").pack()
+tk.Button(win,text="Login using existing account",width=50).pack()
+tk.Button(win,text="Create a new account",width=50,command=account_create).pack()
+
+
+
+win.mainloop()
+
 
 choices = ("Y", "N")
-dbfunc()
+
 while True:
 
     user_option = input("Hi, would you like to create an account? Y/N: ")
